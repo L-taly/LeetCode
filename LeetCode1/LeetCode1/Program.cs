ede2fc8be61d2dadf031f7d862087d2ad212fbe3
L-taly <litanlinlitanlin@outlook.com>
2017-03-21 02:08:30 +0800

    Leetcode1 twosum

---
+++ LeetCode1/LeetCode1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode1
+{
+    class Program
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+            int[] array = new int[2];
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        array[0] = i;
+                        array[1] = j;
+                        break;
+                    }
+                }
+            }
+            return array;
+        }
+
+    static void Main(string[] args)
+        {
+            Console.ReadKey();
+        }
+    }
+}
